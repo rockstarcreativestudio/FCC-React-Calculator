afc2729d6c32211585bc3ce0bef9d4c11713a30d
rockstarcreativestudio <support@rockstarcreativestudio.com>
2019-05-15 14:38:17 -0400

    Correct name of style import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import  React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import 'style.css';
+import 'index.css';
 
 class CalcButtons extends Component {
   constructor(props) {
